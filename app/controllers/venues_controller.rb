db4a187f6955b15751107e7b50be1dac4ec895ce
Ankit Jaini <ajaini@chicagobooth.edu>
2018-12-12 22:54:35 +0000

    Venues page pagination removed and filtered for current user

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,15 +1,13 @@
 class VenuesController < ApplicationController
   def index
-    @q = Venue.ransack(params.fetch("q", nil))
-    @venues = @q.result(:distinct => true).includes(:bookmarks, :neighborhood, :fans, :specialties).page(params.fetch("page", nil)).per(10)
+    #@q = Venue.ransack(params.fetch("q", nil))
+    @q = current_user.bookmarked_venues.ransack(params.fetch("q", nil))
+    @venues = @q.result(:distinct => true).includes(:bookmarks, :neighborhood, :fans, :specialties).page(params.fetch("page", nil)).per(1000)
 
     @location_hash = Gmaps4rails.build_markers(@venues.where.not(:address_latitude => nil)) do |venue, marker|
       marker.lat venue.address_latitude
       marker.lng venue.address_longitude
       marker.infowindow "<h5><a href='/venues/#{venue.id}'>#{venue.created_at}</a></h5><small>#{venue.address_formatted_address}</small>"
-      
-      
-
   
     end
 
